9095fb0fc2ef9e8ed4902d9b7c86c524361249ac
Paul25 <paul@vingcinq.io>
2020-02-06 14:14:23 +0100

    register carousel component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyCarousel :imgs="carouselImagesUrls"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyCarousel from './components/MyCarousel.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    MyCarousel
+  },
+  data() {
+    return {
+      carouselImagesUrls: [
+        'assets/img/photo-1.jpg',
+        'assets/img/photo-2.jpg',
+        'assets/img/photo-3.jpg',
+        'assets/img/photo-4.jpg',
+        'assets/img/photo-5.jpg',
+      ]
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
 }
 </style>
---
+++ src/components/MyCarousel.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <button>Open gallery</button>
+      <button>Previous</button>
+      <button>Next</button>
+      <button>Close</button>
+      <div>
+        <img v-bind:key="index" v-for="(url, index) in imgs" :src="url" alt="" width="200"/>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyCarousel',
+  props: {
+    imgs: {
+        type: Array,
+        default: () => []
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
